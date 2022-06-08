3d6ab0407334e39b972e27c39dcbfb92aa55e7d8
Sahana karishetti karishetti <96462274+Sahanakarishetti1@users.noreply.github.com>
2022-06-08 10:36:27 +0000

    9 completed

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,22 @@
 # Lists
 
+
+
+lst = list()
 filename = "dataset/romeo.txt"
+fname = input(filename)
+
+fh = open(filename)
+
+for line in fh:
+
+    for word in line.split( ):
+
+        if word not in lst:
+
+            lst.append(word)
+
+lst.sort()
+
+print(lst)
+  
